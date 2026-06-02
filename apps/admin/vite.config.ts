6a66959d5894ac0a25b97533d59db00a7ee135b9
xpow <xpow@example.com>
2026-06-02 18:06:17 +0800

    chore: update admin HMR config

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -5,11 +5,16 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5178,
+    host: '0.0.0.0', // 允许外部网络访问
+    cors: true,      // 允许跨域
     hmr: {
       protocol: 'wss',                  // 强制使用加密的 WebSocket
-      host: 'admin-diancan.xpow.cn',   // 你的公网域名
-      port: 443                        // 走 443 端口，交给 Nginx 转发
+      clientPort: 9002,                 // 走 443 端口，交给 Nginx 转发
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
     },
+    allowedHosts:['admin-diancan.xpow.cn'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
